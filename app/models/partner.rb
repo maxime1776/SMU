9578d5fd12cdddb7d7b3df202988c5ed5dfe81d6
Arthur Fulconis <arthur.fulconis@edu.escpeurope.eu>
2015-11-10 15:07:21 +0100

    Progres sur partner controller

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,3 +1,15 @@
 class Partner < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
+
+  validates :first_name, presence: true, uniqueness: true
+  validates :last_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
+  validates :birth_date, presence: true, uniqueness: true
+  validates :birth_location, presence: true, uniqueness: true
+  validates :nationality, presence: true, uniqueness: true
+  validates :admin, presence: true, uniqueness: true
+
+
 end
