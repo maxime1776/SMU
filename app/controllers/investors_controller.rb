80bc7d7d569ec72bf744f8f5883423145eb7ff0e
Emmanuelle Sadone <emmanuellesadone@gmail.com>
2015-11-10 15:18:43 +0100

    contract fixed

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -1,4 +1,58 @@
 class InvestorsController < ApplicationController
 
+  before_action :set_booking, only: [:destroy, :edit, :update]
+  before_action :set_annonce, except: [:edit, :update]
+
+
+  def new
+    @investor = Investors.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.annonce = @annonce
+    @booking.user = current_user
+    @booking.save
+      if @booking.save
+        redirect_to booking_path(@booking)
+      else
+        render :new
+      end
+  end
+
+
+
+  def edit
+    @annonce = @booking.annonce
+  end
+
+  def update
+    @booking.update(booking_params)
+    if @booking.save
+      redirect_to annonces_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking.delete
+    redirect_to root_path
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_annonce
+    @annonce = Annonce.find(params[:annonce_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit( :message, :start_at, :finish_at)
+  end
+
 
 end
--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,33 +1,13 @@
 class Investor < ActiveRecord::Base
   belongs_to :contract
+
+  validates :contract_id, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :nationality, presence: true
+  validates :address, presence: true
+  validates :amount_raised, presence: true
 end
-
-
-
-
-#   validates :user, presence: true
-#   validates :annonce, presence: true
-#   validates :start_at, presence: true
-#   validates :finish_at, presence: true
-
-
-#   validates :user_id, presence: true
-
-#   geocoded_by :adresse
-#   after_validation :geocode, if: :adresse_changed?
-# t.string   "first_name"
-# t.string   "last_name"
-# t.string   "nationality"
-# t.string   "address"
-# t.integer  "amount_raised"
-# t.datetime "created_at",    null: false
-# t.datetime "updated_at",    null: false
-
-
-
-
-
-
 
 
 
