ed2bc4c06fb9888259968295a858e4b391fa927d
Maxime Santilli <maxime1776@gmail.com>
2015-11-10 14:57:51 +0100

    okok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update]
   skip_before_action :authenticate_user!, only: :show
 
   def show
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit( :email, :password)
   end
 
 
