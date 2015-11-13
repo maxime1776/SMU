e4e14062b3d9a86007f45c2480e72e2f9bc77708
Maxime Santilli <maxime1776@gmail.com>
2015-11-13 01:18:40 +0100

    signin et profil

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -17,45 +17,7 @@ def create
   @contract = Contract.new(contract_params)
   @contract.user = current_user
   @contract.save
-    if @contract.save
-      signature_request = HelloSign.send_signature_request_with_template(
-      test_mode: Rails.env.production? ? 0 : 1,
-      template_id: "7491512dbd8562c741319fe8c31b675ee4427e06",
-      title: "#{@card.full_name} - #{@contract_template.title}",
-      subject: @contract_template.subject,
-      message: @contract_template.message,
-      # signing_redirect_url: "",     # TODO: special page on le wagon website?
-      signers: [
-        {
-          email_address: @card.email,
-          name: @card.full_name,
-          role: @contract_template.signer_role
-        }
-      ],
-      # :ccs => [
-      #   {
-      #     :email_address =>'accounting@example.com',
-      #     :role => "Accounting"
-      #   }
-      # ],
-      custom_fields: {  # run `rake hello_sign:list_templates` to get those
-        student_name: @card.full_name,
-        student_phone_number: @card.phone_number,
-        student_email: @card.email,
-        starts_at_one: starts_at,
-        ends_at_one: ends_at,
-        starts_at_two: starts_at,
-        ends_at_two: ends_at,
-        price_number_excluding_vat: price_number_excluding_vat,
-        price_number_vat: price_number_vat,
-        price_number_total: price_number_total,
-        price_letters_total: I18n.with_locale(@card.board.locale) { price_number_total.to_words },
-        deposit_number_total: deposit_number_total
-      }
-     )
-      redirect_to root_path
-    else
-    end
+
 end
 
 def edit
