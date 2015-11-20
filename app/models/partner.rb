d8ef3755571de317a62ec5672174ea91607e57f8
Arthur Fulconis <arthur.fulconis@edu.escpeurope.eu>
2015-11-20 14:03:50 +0100

    Final steps

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,20 +2,20 @@ class Partner < ActiveRecord::Base
   belongs_to :user, dependent: :destroy
   belongs_to :contract
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :phone, presence: true
-  validates :address, presence: true
-  validates :birth_date, presence: true
-  validates :birth_location, presence: true
-  validates :nationality, presence: true
-  validates :founders_receivables, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :email, presence: true
+  # validates :phone, presence: true
+  # validates :address, presence: true
+  # validates :birth_date, presence: true
+  # validates :birth_location, presence: true
+  # validates :nationality, presence: true
+  # validates :founders_receivables, presence: true
 
 
 
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  validates_format_of :phone, :with => /\A0\d{9}\z/
+  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  # validates_format_of :phone, :with => /\A0\d{9}\z/
   # validates_format_of :phone, with: /^0\d{9}$/ :multiline => true option?
 
   # validates :email, presence: true, uniqueness: true
